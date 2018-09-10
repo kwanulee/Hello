c5f482d0ff16dc089f3fd7f183e9e81b686542b5
kwanulee <kwlee@hansung.ac.kr>
2018-09-11 07:19:20 +0900

    초기 커밋

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int year = 2017;
+        System.out.println("Welcome to "+year+" Java Class");
+    }
+}
