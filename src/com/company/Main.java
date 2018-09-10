84c297280ceceb6909304a4e3ef9a1b257f71e35
kwanulee <kwlee@hansung.ac.kr>
2018-09-11 07:26:51 +0900

    연도 및 이름 수정

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int year = 2017;
-        System.out.println("Welcome to "+year+" Java Class");
+        int year = 2018;
+        System.out.println("Welcome to "+year+" Web&App Class");
     }
 }
